8476841d55fc090beadc06b9558b130cbaad43bb
Ralph <my@ralphsun.me>
2024-11-26 18:00:02 +0800

    Changed: README

--- src/lib/AddTodo.svelte
+++ src/lib/AddTodo.svelte
@@ -1,9 +1,11 @@
 <script>
 	"use strict";
+
+	let addTodo = $state("");
 </script>
 
 <div class="card input">
-	<input type="text" placeholder="請輸入待辦任務" />
+	<input type="text" placeholder="請輸入待辦任務" bind:value={addTodo} />
 	<a href="/" class="btn_add">+</a>
 </div>
 
